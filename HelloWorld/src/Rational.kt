e5b2acede6a962677ea6843567339c37bf74fa3c
上原和輝 <qwe159753qwee@gmail.com>
2018-02-15 21:53:16 +0900

    Finish 3-2

--- HelloWorld/src/Rational.kt
+++ HelloWorld/src/Rational.kt
@@ -1,4 +1,6 @@
 /**
  * Created by nappannda on 2018/02/15.
  */
-class Rational(val numerator: Int, val denominator: Int)
+class Rational(val numerator: Int, val denominator: Int) {
+    override fun toString(): String = "${numerator}/${denominator}"
+}

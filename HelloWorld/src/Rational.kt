a8439f47a01aaa55ce027982f4da89a878ccf7cd
上原和輝 <qwe159753qwee@gmail.com>
2018-02-15 22:15:37 +0900

    Finish 3-5

--- HelloWorld/src/Rational.kt
+++ HelloWorld/src/Rational.kt
@@ -2,13 +2,23 @@
  * Created by nappannda on 2018/02/15.
  */
 class Rational(n: Int, d: Int) {
+
     init {
         require(d != 0, {"denominator must not be null"})
     }
-    private val g = gcd(Math.abs(n), Math.abs(d))
-    val numerator: Int = n / g
-    val denominator: Int = d / g
+
+    private val g by lazy { gcd(Math.abs(n), Math.abs(d)) }
+    val numerator: Int by lazy { n / g }
+    val denominator: Int by lazy { d / g }
+
+    operator fun plus(that: Rational): Rational =
+        Rational(
+                numerator * that.denominator + that.numerator * denominator,
+                denominator * that.denominator
+        )
+
     override fun toString(): String = "${numerator}/${denominator}"
+
     tailrec private fun gcd(a: Int, b: Int): Int =
             if (b == 0) a
             else gcd(b, a % b)

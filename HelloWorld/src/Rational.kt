87f0feeca289f05ca3b6303031fe3862cb8ad5fc
上原和輝 <qwe159753qwee@gmail.com>
2018-02-15 22:07:38 +0900

    Finish 3-4

--- HelloWorld/src/Rational.kt
+++ HelloWorld/src/Rational.kt
@@ -1,9 +1,15 @@
 /**
  * Created by nappannda on 2018/02/15.
  */
-class Rational(val numerator: Int, val denominator: Int) {
+class Rational(n: Int, d: Int) {
     init {
-        require(denominator != 0, {"denominator must not be null"})
+        require(d != 0, {"denominator must not be null"})
     }
+    private val g = gcd(Math.abs(n), Math.abs(d))
+    val numerator: Int = n / g
+    val denominator: Int = d / g
     override fun toString(): String = "${numerator}/${denominator}"
+    tailrec private fun gcd(a: Int, b: Int): Int =
+            if (b == 0) a
+            else gcd(b, a % b)
 }

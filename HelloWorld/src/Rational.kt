c7e46817cc04dc2c9ff80e8fed3315d543043140
上原和輝 <qwe159753qwee@gmail.com>
2018-02-15 21:58:22 +0900

    Finish 3-3

--- HelloWorld/src/Rational.kt
+++ HelloWorld/src/Rational.kt
@@ -2,5 +2,8 @@
  * Created by nappannda on 2018/02/15.
  */
 class Rational(val numerator: Int, val denominator: Int) {
+    init {
+        require(denominator != 0, {"denominator must not be null"})
+    }
     override fun toString(): String = "${numerator}/${denominator}"
 }

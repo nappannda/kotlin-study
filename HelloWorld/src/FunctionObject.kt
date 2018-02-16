411f8902f771c8a9144b6f415045c960c42e523c
上原和輝 <qwe159753qwee@gmail.com>
2018-02-17 03:33:40 +0900

    Finish 6-3

--- HelloWorld/src/FunctionObject.kt
+++ HelloWorld/src/FunctionObject.kt
@@ -1,3 +1,5 @@
+import com.sun.org.apache.xpath.internal.operations.Bool
+
 /**
  * Created by nappannda on 2018/02/17.
  */
@@ -6,4 +8,24 @@ fun square(i: Int): Int = i * i
 fun main(args: Array<String>) {
     var functionObject = ::square
     println(functionObject(5))
+}
+
+fun firstK(str: String): Int {
+    fun isK(c: Char): Boolean = c == 'K'
+    return first(str, ::isK)
+}
+
+fun firstUpperCase(str: String): Int {
+    fun isUpperCase(c: Char): Boolean = c.isUpperCase()
+    return first(str, ::isUpperCase)
+}
+
+fun first(str: String, predicate: (Char) -> Boolean): Int {
+    tailrec fun go(str: String, index: Int): Int =
+        when {
+            str.isEmpty() -> 1
+            predicate(str.first()) -> index
+            else -> go(str.drop(1), index + 1)
+        }
+    return go(str, 0)
 }

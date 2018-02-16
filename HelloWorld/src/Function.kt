ac21c665ab3aaa08f470db5f803f84d19225d5e6
上原和輝 <qwe159753qwee@gmail.com>
2018-02-16 20:53:33 +0900

    Finish 5-5

--- HelloWorld/src/Function.kt
+++ HelloWorld/src/Function.kt
@@ -1,3 +1,5 @@
+import java.util.concurrent.atomic.DoubleAccumulator
+
 /**
  * Created by nappannda on 2018/02/16.
  */
@@ -7,3 +9,7 @@ fun main(args: Array<String>) {
     val result = succ(31)
     println(result)
 }
+
+tailrec fun sum(numbers: List<Long>, accumulator: Long = 0): Long =
+    if (numbers.isEmpty()) accumulator
+    else sum(numbers.drop(1), accumulator + numbers.first())

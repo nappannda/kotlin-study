51c66c79a42737e026465ecd71cd356ce68ca201
上原和輝 <qwe159753qwee@gmail.com>
2018-02-18 21:14:01 +0900

    Finish 8-2

--- HelloWorld/src/ClassMember.kt
+++ HelloWorld/src/ClassMember.kt
@@ -6,4 +6,13 @@ class MyClass
 fun main(args: Array<String>) {
     val myClass: MyClass = MyClass()
     println(myClass)
+
+    val greeter = Greeter()
+    greeter.greet("Kotlin")
+}
+
+class Greeter {
+    fun greet(name: String) {
+        println("Hello, $name!")
+    }
 }

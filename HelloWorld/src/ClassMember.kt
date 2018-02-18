c89ad42d476705edb3542130f9ec4d500285ff48
上原和輝 <qwe159753qwee@gmail.com>
2018-02-18 21:19:58 +0900

    Finish 8-3

--- HelloWorld/src/ClassMember.kt
+++ HelloWorld/src/ClassMember.kt
@@ -1,7 +1,9 @@
 /**
  * Created by nappannda on 2018/02/18.
  */
-class MyClass
+class MyClass {
+    lateinit var foo: String
+}
 
 fun main(args: Array<String>) {
     val myClass: MyClass = MyClass()
@@ -9,10 +11,35 @@ fun main(args: Array<String>) {
 
     val greeter = Greeter()
     greeter.greet("Kotlin")
+
+    val hanako = Person()
+
+    println(hanako.name)
+    println(hanako.age)
+
+    hanako.name = "はなこ"
+    hanako.age = 25
+
+    println(hanako.name)
+    println(hanako.age)
+    println(hanako.nameLength)
 }
 
 class Greeter {
     fun greet(name: String) {
         println("Hello, $name!")
     }
+}
+
+class Person {
+    var name: String = ""
+        set(value) {
+            println("${value}がセットされました。")
+            field = value
+        }
+    var age: Int = 0
+    val nameLength: Int
+        get(): Int {
+            return this.name.length
+        }
 }

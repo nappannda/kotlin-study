ccebbfc270ecff4265c80a4eb92b758cbb2c7f00
上原和輝 <qwe159753qwee@gmail.com>
2018-02-19 23:53:12 +0900

    Finish 11-2

--- HelloWorld/src/Generics.kt
+++ HelloWorld/src/Generics.kt
@@ -2,3 +2,8 @@
  * Created by nappannda on 2018/02/19.
  */
 class Container<T>(var value: T)
+
+fun <T> box(value: T): Container<T> = Container(value)
+
+val<T> T.string: String
+    get() = toString()

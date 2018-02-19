4c97b3b908dba69f2cdb73db47766889cfd44e74
上原和輝 <qwe159753qwee@gmail.com>
2018-02-19 23:11:14 +0900

    Finish 10-3

--- HelloWorld/src/Interface.kt
+++ HelloWorld/src/Interface.kt
@@ -1,55 +1,70 @@
-/**
- * Created by nappannda on 2018/02/19.
- */
-interface Greeter {
-    val language: String
-    fun sayHello(target: String)
-}
-
-class EnglishGreeter : Greeter {
-    override val language = "English"
-    override fun sayHello(target: String) {
-        println("Hello, $target!")
-    }
-}
-
-open class SuperClass
+///**
+// * Created by nappannda on 2018/02/19.
+// */
+//interface Greeter {
+//    val language: String
+//    fun sayHello(target: String)
+//}
+//
+//class EnglishGreeter : Greeter {
+//    override val language = "English"
+//    override fun sayHello(target: String) {
+//        println("Hello, $target!")
+//    }
+//}
+//
+//open class SuperClass
+//
+//interface Foo {
+//    fun execute()
+//}
+//interface Bar {
+//    fun execute()
+//}
+//
+//class FooBar : Foo, Bar {
+//    override fun execute() {
+//        println("FooBar")
+//    }
+//}
+//
+//open class Superclass {
+//    open fun execute() {
+//        println("Superclass")
+//    }
+//}
+//
+//class FooSubclass : Superclass(), Foo
+//
+//interface Hoge {
+//    fun execute() {
+//        println("Hoge")
+//    }
+//}
+//
+//interface Fuga {
+//    fun execute() {
+//        println("Fuga")
+//    }
+//}
+//
+//class HogeFuga : Hoge, Fuga {
+//    override fun execute() {
+//        super<Hoge>.execute()
+//    }
+//}
 
 interface Foo {
-    fun execute()
-}
-interface Bar {
-    fun execute()
+    fun aaa()
+    fun bbb()
 }
 
-class FooBar : Foo, Bar {
-    override fun execute() {
-        println("FooBar")
-    }
+interface Bar: Foo {
+    override fun aaa() {}
+    fun ccc()
 }
 
-open class Superclass {
-    open fun execute() {
-        println("Superclass")
-    }
-}
-
-class FooSubclass : Superclass(), Foo
-
-interface Hoge {
-    fun execute() {
-        println("Hoge")
-    }
-}
-
-interface Fuga {
-    fun execute() {
-        println("Fuga")
-    }
-}
-
-class HogeFuga : Hoge, Fuga {
-    override fun execute() {
-        super<Hoge>.execute()
-    }
+class Baz: Bar {
+    override fun bbb() {}
+    override fun ccc() {}
 }

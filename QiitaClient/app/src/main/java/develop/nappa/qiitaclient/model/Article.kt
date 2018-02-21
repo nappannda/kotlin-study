103733e21766cb0e85bb5632f9f965cb9163c660
上原和輝 <qwe159753qwee@gmail.com>
2018-02-21 19:37:03 +0900

    Finish 15-1

---
+++ QiitaClient/app/src/main/java/develop/nappa/qiitaclient/model/Article.kt
@@ -0,0 +1,9 @@
+package develop.nappa.qiitaclient.model
+
+/**
+ * Created by nappannda on 2018/02/21.
+ */
+data class Article(val id: String,
+                   val title: String,
+                   val url: String,
+                   val user: User)
---
+++ QiitaClient/app/src/main/java/develop/nappa/qiitaclient/model/User.kt
@@ -0,0 +1,8 @@
+package develop.nappa.qiitaclient.model
+
+/**
+ * Created by nappannda on 2018/02/21.
+ */
+data class User(val id: String,
+                val name: String,
+                val profileImageUrl: String)

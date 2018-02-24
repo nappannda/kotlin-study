041cc8182b5548247a3054a9f3858b86add38cdc
上原和輝 <qwe159753qwee@gmail.com>
2018-02-24 20:27:49 +0900

    Finish 17-3

--- QiitaClient/app/src/androidTest/java/develop/nappa/qiitaclient/MainActivityTest.kt
+++ QiitaClient/app/src/androidTest/java/develop/nappa/qiitaclient/MainActivityTest.kt
@@ -1,17 +1,22 @@
 package develop.nappa.qiitaclient
 
 import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.action.ViewActions.typeText
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.view.View
+import develop.nappa.qiitaclient.client.ArticleClient
 import org.hamcrest.Matcher
 import org.hamcrest.Matchers.not
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito.*
+import rx.Observable
 
 /**
  * Created by nappannda on 2018/02/24.
@@ -34,4 +39,16 @@ class MainActivityTest {
 
     fun isNotDisplayed(): Matcher<View> = not(isDisplayed())
 
+    @Test
+    fun `検索ボタンがタップされたら、入力されたクエリ文字列で記事検索APIを叩くこと`() {
+        val articleClient = mock(ArticleClient::class.java).apply {
+            `when`(search("user:ngsw_taro")).thenReturn(Observable.just(listOf()))
+        }
+        activityTestRule.activity.articleClient = articleClient
+
+        onView(withId(R.id.query_edit_text)).perform(typeText("user:ngsw_taro"))
+        onView(withId(R.id.search_button)).perform(click())
+
+        verify(articleClient).search("user:ngsw_taro")
+    }
 }
